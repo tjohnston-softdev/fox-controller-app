4696046e95ac648ea05085fe552ac924e252ec82
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-04-09 15:34:51 +1000

    Remote IO Factories
    Wrote emulation for Remote IO Factories.

--- changelog.md
+++ changelog.md
@@ -2,16 +2,9 @@
 
 **Test Status**
 * G - Controller Files
-	* All 'Connected Device Class' tests pass.
-	* 'ConnectedDevice' class emulation is complete.
+	* Remote IO Factories tests all pass.
 
 ---
 
-**./fox-devices/_classes/device.class.js**
-* 'ConnectedDevice' constructor.
-	* Renamed '_storedDevice' parameter to 'storeDeviceObj'
-	* Moved instance type validation to its own function 'handleClassType'
-	* 'storedDevice' property is now set after validation passes.
-* New function 'handleModel'
-	* Attempts to retrieve model object by name.
-	* If no such model exists, an error will be thrown.
+**./fox-devices/remote_io/remote-io.factories.js**
+* Wrote emulation for script file.
--- fox-devices/remote_io/remote-io.factories.js
+++ fox-devices/remote_io/remote-io.factories.js
@@ -1,1 +1,32 @@
-console.log("Remote IO Factories");
+const rioSettings = require("./remote_io.settings");
+const deviceClass = require("../_classes/device.class");
+const modelClass = require("../_classes/device-model.class");
+
+
+function createRemoteIoModule(inputObject)
+{
+	var storedDeviceObject = new modelClass.StoredDevice(inputObject);
+	var prepRes = {};
+	
+	prepRes.maker = storedDeviceObject.maker;
+	prepRes.ipAddress = storedDeviceObject.ipAddress;
+	prepRes.disableRio = remoteIoPlaceholder;
+	prepRes.getRioDeviceProperties = remoteIoPlaceholder;
+	prepRes.registerNodeCallback = remoteIoPlaceholder;
+	prepRes.setOutputFromNode = remoteIoPlaceholder;
+	prepRes.getCommsErrors = remoteIoPlaceholder;
+	
+	return prepRes;
+}
+
+
+function remoteIoPlaceholder()
+{
+	return true;
+}
+
+
+module.exports =
+{
+	RemoteIoModule: createRemoteIoModule
+};
