076dbd7fddee81652fc36f676a5f35f9121340ad
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-04-09 15:06:19 +1000

    Restructured 'StoredDevice' class
    * Rewrote 'StoredDevice' class so property changes are
    correctly validated.
    
    * All 'Stored Device Class' tests are successful.
    
    * Most 'Connected Device Class' tests are successful.

--- changelog.md
+++ changelog.md
@@ -1,45 +1,24 @@
 # Changelog
 
+**Test Status**
+* G - Controller Files
+	* All 'Stored Device Class' tests are successful.
+		* 'StoredDevice' class emulation is complete for now.
+	* Most 'Connected Device Class' tests are successful.
+		* This is because most invalid tests change 'StoredDevice' properties.
+		* Property changes are now correctly intercepted and validated.
+		* 'Valid Model' test still fails as it has not been directly addressed.
+		* 'Invalid Model' fails because it is handled differently compared to above.
+
+---
+
 **./fox-devices/_classes/device-model.class.js**
-* Removed setter comment.
-* Removed property input validation.
-	* Only the base type is checked.
-	* 'id' property is given a placeholder value.
-
----
-
-**./fox-devices/_classes/device.class.js**
-* Removed 'handlePropertyValidation' function.
-
----
-
-**./fox-custom/validation-tasks.js - Renamed Functions**
-* 'readStringValueProperty' to 'checkStringProperty'
-* 'readBooleanValueProperty' to 'checkBooleanProperty'
-* 'readNumberValueProperty' to 'checkNumberProperty'
-* 'readDeviceTypeValueProperty' to 'checkDeviceTypeProperty'
-* 'readReferenceStringValueProperty' to 'checkReferenceStringProperty'
-* 'readIpAddressValueProperty' to 'checkIpAddressProperty'
-
----
-
-**./fox-custom/validation-tasks.js - Function Changes**
-* writePropertyTypeError
-	* Added new parameter 'vAction'
-	* Format: `...in %vClass% during %vAction%`
-* checkStringProperty
-	* Added new parameter 'actionDesc'.
-* checkBooleanProperty, checkNumberProperty
-	* Removed 'inputObj' parameter.
-	* 'propValue' is now a parameter.
-	* Added new parameter 'actionDesc'
-* checkDeviceTypeProperty
-	* Removed 'inputObj' parameter.
-	* 'targetValue' is now a parameter.
-* checkReferenceStringProperty
-	* Removed 'propValue' parameter.
-	* 'targetValue' is now a parameter.
-* checkIpAddressProperty
-	* Removed 'inputObj' parameter.
-	* 'propValue' is now a parameter.
-
+* Rewrote 'StoredDevice' class
+	* Base object validation remains as-is
+	* Property validation is reintroduced.
+	* Constructor returns a proxy object (getUpdateObject)
+* Wrote new function 'getUpdateObject'
+	* Adds event monitoring to 'StoredDevice' class object.
+	* Whenever a property value is set, 'handlePropertyUpdate' is called.
+* Wrote new function 'handlePropertyUpdate'
+	* Validates 'StoredDevice' property when changed.
--- fox-devices/_classes/device-model.class.js
+++ fox-devices/_classes/device-model.class.js
@@ -7,10 +7,65 @@ class StoredDevice
 {
 	constructor(inputDeviceObj)
 	{
+		var conName = this.constructor.name;
+		var validContext = "construction";
+		
 		validationTasks.checkBaseObject(inputDeviceObj, "device");
-		this.id = "123";
-		return this;
+		this.id = validationTasks.checkStringProp("id", inputDeviceObj.id, null, conName, validContext);
+		this.deviceType = validationTasks.checkDeviceTypeProp("deviceType", inputDeviceObj.deviceType, deviceSettings.deviceType.remoteIo);
+		this.maker = validationTasks.checkReferenceStringProp("maker", inputDeviceObj.maker, deviceSettings.listRioMakers);
+		this.model = validationTasks.checkReferenceStringProp("model", inputDeviceObj.model, deviceSettings.listRioModelTypes);
+		this.name = validationTasks.checkStringProp("name", inputDeviceObj.name, null, conName, validContext);
+		this.desc = validationTasks.checkStringProp("desc", inputDeviceObj.desc, "", conName, validContext);
+		this.ipAddress = validationTasks.checkIpAddressProp("ipAddress", inputDeviceObj.ipAddress);
+		this.username = validationTasks.checkStringProp("username", inputDeviceObj.username, "", conName, validContext);
+		this.password = validationTasks.checkStringProp("password", inputDeviceObj.password, "", conName, validContext);
+		this.isEnabled = validationTasks.checkBooleanProp("isEnabled", inputDeviceObj.isEnabled, null, conName, validContext);
+		this.macAddress = validationTasks.checkStringProp("macAddress", inputDeviceObj.macAddress, null, conName, validContext);
+		this.isDeleted = validationTasks.checkBooleanProp("isDeleted", inputDeviceObj.isDeleted, false, conName, validContext);
+		this.__modified = validationTasks.checkNumberProp("__modified", inputDeviceObj.__modified, 0, conName, validContext);
+		
+		return getUpdateObject(this);
 	}
+}
+
+
+function getUpdateObject(cObject)
+{
+	var updateRes = new Proxy(cObject,
+	{
+		set: handlePropertyUpdate
+	});
+	
+	return updateRes;
+}
+
+
+
+function handlePropertyUpdate(parentObject, updKey, updValue)
+{
+	if (updKey === "id" || updKey === "deviceType" || updKey === "name" || updKey === "ipAddress" || updKey === "macAddress")
+	{
+		parentObject[updKey] = validationTasks.checkStringProp(updKey, updValue, null, "StoredDevice", "setting");
+	}
+	else if (updKey === "maker" || updKey === "model")
+	{
+		parentObject[updKey] = validationTasks.checkStringProp(updKey, updValue, null, "StoredDevice", "setting");
+	}
+	else if (updKey === "isEnabled" || updKey === "isDeleted")
+	{
+		parentObject[updKey] = validationTasks.checkBooleanProp(updKey, updValue, null, "StoredDevice", "setting");
+	}
+	else if (updKey === "__modified")
+	{
+		parentObject[updKey] = validationTasks.checkNumberProp(updKey, updValue, 0, "StoredDevice", "setting");
+	}
+	else
+	{
+		parentObject[updKey] = validationTasks.checkStringProp(updKey, updValue, "", "StoredDevice", "setting");
+	}
+	
+	return true;
 }
 
 module.exports =
