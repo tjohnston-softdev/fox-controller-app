5dd4d1a2baf8d56e0eee75347c2e40c450f3a4a5
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-04-13 12:56:49 +1000

    'Service.Main' emulation
    Wrote most emulation for the 'service.main' file. Only
    rebooting and factory reset remain. The tests only
    check whether the function exists but they will
    most likely be required for the APIs.

--- changelog.md
+++ changelog.md
@@ -1,3 +1,12 @@
 # Changelog
 
-All Remote IO index tests pass
+**Test Status**
+* G - Controller Files
+	* Wrote most emulation for the 'service.main' file.
+	* Only rebooting and factory reset remain.
+	* The tests only check whether the function exists but they will most likely be required for the APIs.
+
+---
+
+**./service.main.js**
+* Wrote most emulation for this file.
--- service.main.js
+++ service.main.js
@@ -1,1 +1,72 @@
-console.log("Services Main");
+const restartTime = 2000;
+
+class Controller
+{
+	constructor()
+	{
+		this.temperature = -1;
+        this.humidity = -1;
+	}
+	
+	getHealth(healthCallback)
+	{
+		var healthRes = {};
+		return healthCallback(healthRes);
+	}
+	
+	getEnvironment(envCallback)
+	{
+		var randTemp = Math.random() * 100;
+		var randHumid = Math.random() * 100;
+		var environmentRes = {};
+		
+		environmentRes["temperature"] = Math.round(randTemp * 10) / 10;
+		environmentRes["humidity"] = Math.round(randHumid * 10) / 10;
+		
+		return envCallback(environmentRes);
+	}
+	
+	getDiskSpace(diskCallback)
+	{
+		var randBytes = Math.random() * 500000000000;
+		return diskCallback(randBytes);
+	}
+	
+	getDatabaseSize(sizeCallback)
+	{
+		var randBytes = Math.random() * 100000;
+		return sizeCallback(randBytes);
+	}
+	
+	getLogSize(logCallback)
+	{
+		var randBytes = Math.random() * 50000;
+		return logCallback(randBytes);
+	}
+	
+	stopFlows(stopCallback)
+	{
+		stopCallback();
+	}
+	
+	startFlows(startCallback)
+	{
+		startCallback();
+	}
+	
+	restartProcess()
+	{
+		setTimeout(function()
+		{
+			process.exit();
+		}, restartTime);
+	}
+}
+
+
+const controllerObject = new Controller();
+
+module.exports =
+{
+	controller: controllerObject
+};
