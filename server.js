ddc5673cbab6aa80397a17504127b48caafe58af
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-04-14 10:43:42 +1000

    Removed '/users' route.
    This was part of the express generator.

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var adminRouter = require("./routes/admin");
 
 var app = express();
@@ -16,7 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/admin', adminRouter);
 
 module.exports = app;
