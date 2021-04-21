12150abf26c49fc928afd6dd3a2259e9c7414673
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-04-21 11:00:17 +1000

    Deleted original RIO Modify file
    ./fox-custom/rio-modify.js

--- fox-custom/rio-modify.js
+++
@@ -1,4 +0,0 @@
-const series = require("run-series");
-const deviceModelClass = require("../fox-devices/_classes/device-model.class");
-const deviceSettings = require("../fox-devices/device.settings");
-const rioFactories = require('../fox-devices/remote_io/remote-io.factories');
