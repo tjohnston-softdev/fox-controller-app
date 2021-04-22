a5482c4fffae0973a23a83ae046a6362f0679c09
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-04-23 09:52:39 +1000

    Cleaned 'Log Size' code
    Removed or merged redundant log size generation code.

--- changelog.md
+++ changelog.md
@@ -1,12 +1,8 @@
 # Changelog
 
-**./fox-api/alarm-database.js**
-* Renamed 'getAlarmObjects' to 'getPopulatedAlarmObjects'
-	* Called publicly as 'getPopulatedAlarms'
-	* Uncommented `queryRes.push`
-	* Returns array of alarm objects that match input parameters.
-* Wrote new function 'getPopulatedAvailabilityObjects'
-	* GET function for 'available' global.
-	* Returns array of alarm availability objects.
-	* Called publicly as 'getPopulatedAvailable'
-* Rewrote 'getAlarmObjects' to only return an empty array.
+**./fox-api/log-size.js**
+* Added requirement for '../fox-custom/folder-item'
+* getLogSizeObject
+	* Declared 'logSizeBytes' variable - Stores random log size.
+	* Declared 'logUpdateTime' variable - Stores current time.
+	* 'infoObject' is assigned by calling `folderItem.defineItem`
--- fox-api/log-size.js
+++ fox-api/log-size.js
@@ -1,17 +1,16 @@
 const sizeFactors = require("../fox-custom/size-factors");
 const randomValues = require("../fox-custom/random-values");
+const folderItem = require("../fox-custom/folder-item");
 
 
 function getLogSizeObject()
 {
-	var infoObject = {};
+	var logSizeBytes = randomValues.generateVolume(8, sizeFactors.KB);
+	var logUpdateTime = Date.now();
+	var logCreateTime = 1524710340000;			// 2018-04-26 12:39:00
 	
-	infoObject["name"] = "fox-controller.log";
-	infoObject["size"] = randomValues.generateVolume(8, sizeFactors.KB);
-	infoObject["isDirectory"] = false;
-	infoObject["modified"] = Date.now();
-	infoObject["created"] = 1524710340000;			// 2018-04-26 12:39:00
 	
+	var infoObject = folderItem.defineItem("fox-controller.log", logSizeBytes, false, logUpdateTime, logCreateTime);
 	var logRes = [infoObject];
 	return logRes;
 }
